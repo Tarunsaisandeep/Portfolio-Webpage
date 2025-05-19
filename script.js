f96d521a8cb7987a3e3d3760b61bb17b6900271f
Tarunsaisandeep <sandeepanala03@gmail.com>
2025-05-19 17:27:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+// Certificates Section Script
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+    constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery() {
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i + 1}`);
+        });
+    }
+
+    setCurrentState(direction) {
+        if (direction.className === 'gallery-controls-previous') {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls() {
+        this.carouselControls.forEach(control => {
+            const button = document.createElement('button');
+            button.className = `gallery-controls-${control}`;
+            button.innerText = control;
+            galleryControlsContainer.appendChild(button);
+        });
+    }
+
+    useControls() {
+        const triggers = [...galleryControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
+
+// Contact Form Script
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const response = await fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        alert("Thank you! I'll get back to you as soon as possible.");
+        form.reset();
+    } else {
+        alert('Oops! There was a problem submitting your Contact form.');
+    }
+});
